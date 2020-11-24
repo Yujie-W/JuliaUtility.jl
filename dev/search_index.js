6ddde4a83c0013b58d2099acc93fa4c001d1c625
Documenter.jl <documenter@juliadocs.github.io>
2020-11-24 04:08:30 +0000

    build based on 73bb45e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#JuliaUtility.jl","page":"Home","title":"JuliaUtility.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Utility functions for Julia.","category":"page"},{"location":"#Installation","page":"Home","title":"Installation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"julia> using Pkg;\njulia> Pkg.add(\"JuliaUtility\");","category":"page"},{"location":"API/#API","page":"API","title":"API","text":"","category":"section"},{"location":"API/","page":"API","title":"API","text":"CurrentModule = JuliaUtility","category":"page"}]
+}
